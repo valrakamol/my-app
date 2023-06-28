42b7a87ce2fbf174ee5320c39277b78886d48e54
valrakamol <valrakamol.kh.65@ubu.ac.th>
2023-06-28 19:19:00 +0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>2023 @Valrakamol Khamsanrach</span>
+        </div>
+    );
+}
+
+export default Footer;
